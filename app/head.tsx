82b2fcaed54f29fd721a6a91b35732a0a4371f76
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-18 22:33:08 +0530

    Now finally it's working

--- app/head.tsx
+++ app/head.tsx
@@ -5,7 +5,7 @@ const head = () => {
   <>
   <script src="https://cdn.jsdelivr.net/gh/greentfrapp/pocoloco@minigl/minigl.js" async></script>
   <title>Hartaj Writes</title>
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
   </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,21 @@ const Home = () => {
     <>
       <div className="flex flex-col justify-center items-center">
         <div className=" w-full sm:w-[80vw] flex flex-col justify-center items-center sm:h-[90vh] ">
-          <div>
-            <Image
-              src={"/Elantepic.jpg"}
-              width={345}
-              height={345}
-              alt="hartaj's pic"
-              className="rounded-[50%]"
-            />
+          <div className="">
+            <img src="/Elantepic.jpg" className="rounded-[50%] h-40 w-40"  alt="" />
           </div>
           <div className="mt-[2rem]">
             {" "}
-            <h1 className="text-[6.4rem] sm:text-7xl  text-white font-['ClashDisplay']">
+            <h1 className="text-[3rem] sm:text-7xl  text-white font-['ClashDisplay']">
               Hartaj <span>&nbsp; Writes</span>
             </h1>{" "}
           </div>
         </div>
-        <div className="bg-[#153250] w-full flex flex-col justify-center items-center text-white font-bold leading-['1'] font-['Unbounded'] text-[3.8rem]  h-[50vh] sm:h-[60vh]">
-          <div className="sm:w-[80vw] w-full">
-         Hartaj Writes , blog by Hartaj Singh </div> <div > Straight Forward and Simple
-            articles </div> <span> on Web Development and Computer Science
-          </span>
+        <div className="bg-[#153250] w-full flex flex-col justify-center items-center text-white font-bold leading-['1'] font-['Unbounded'] text-[2rem]  h-[50vh] sm:h-[60vh]">
+          <div className="w-full">
+         Hartaj Writes , blog by Hartaj Singh </div> <div className="" > Straight Forward and Simple
+            articles </div> <div> on Web Development and Computer Science
+          </div>
           
 
         </div>
