ca57dc354c1ee3094b04c363571b550aa803c436
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-19 19:19:13 +0530

    Fixing Issues in mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,17 @@ const Home = () => {
           </div>
           <div className="mt-[2rem]">
             {" "}
-            <h1 className="text-[3rem] sm:text-7xl  text-white font-['ClashDisplay']">
+            <h1 className="text-[3rem] sm:text-7xl  text-slate-200 font-['ClashDisplay']">
               Hartaj <span>&nbsp; Writes</span>
             </h1>{" "}
           </div>
         </div>
-        <div className="bg-[#153250] px-2 w-full flex flex-col justify-center items-center text-white font-bold leading-[3.2rem] sm:leading-[4.2rem] md:leading-[5.2rem] lg:leading-[6.2rem] xl:leading-[7.2rem] font-['Unbounded'] tracking-tight  text-3xl  xl:text-[5.5rem] sm:text-[3.7rem] lg:text-[4.7rem]  h-[55vh] sm:h-[90vh]">
+        <div className="bg-[#0e2640] px-3 w-full flex flex-col justify-center items-center text-slate-400 font-bold leading-[3.3rem] sm:leading-[4.2rem] md:leading-[5.2rem] lg:leading-[6.2rem] xl:leading-[7.2rem] font-['Unbounded'] tracking-tight  text-3xl  xl:text-[5.5rem] sm:text-[3.7rem] lg:text-[4.7rem]  h-[55vh] sm:h-[90vh]">
           <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]">
-         Hartaj Writes , blog by Hartaj Singh </div>
-          <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]" > Straight Forward and Simple
-            articles </div> 
-            <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]"> on Web Development and Computer Science
+         Hartaj Writes , blog by <span className="text-slate-200"> Hartaj Singh</span> </div>
+          <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]" > <span className="text-slate-200">  Straight Forward </span> and Simple
+         <span className="text-slate-300"> articles</span>  </div> 
+            <div className="w-full mx-1 sm:mx-0 sm:w-[80vw] "> on <span className="text-slate-200"> Web Development</span> and <span className="text-slate-200"> Computer Science</span>
           </div>
           
 
