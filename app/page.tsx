3bb13a2cdb86eb6411fafd8e011cfd60bb912b1d
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-19 23:41:56 +0530

    Adding blog page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Blogs from "../components/Blogs";
 
 const Home = () => {
   return (
@@ -7,7 +8,7 @@ const Home = () => {
       <div className="flex flex-col justify-center items-center">
         <div className=" w-full sm:w-[80vw] flex flex-col justify-center items-center h-screen sm:h-[90vh] ">
           <div className="">
-            <img src="/Elantepic.jpg" className="rounded-[50%] h-40 w-40 sm:h-60 sm:w-60"  alt="" loading="eager" />
+            <img src="/Elantepic.jpg" className="rounded-[50%] h-44 w-44 sm:h-64 sm:w-64"  alt="" loading="eager" />
           </div>
           <div className="mt-[2rem]">
             {" "}
@@ -16,7 +17,7 @@ const Home = () => {
             </h1>{" "}
           </div>
         </div>
-        <div className="bg-[#0e2640] px-3 w-full flex flex-col justify-center items-center text-slate-400 font-bold leading-[3.4rem] sm:leading-[4.2rem] md:leading-[5.2rem] lg:leading-[6.2rem] xl:leading-[7.4rem] font-['Unbounded'] tracking-tight  text-3xl  xl:text-[5.2rem] sm:text-[3.7rem] lg:text-[4.7rem]  h-[55vh] sm:h-screen">
+        <div className="bg-[#0d233b] px-3 w-full flex flex-col justify-center items-center text-slate-400 font-bold leading-[3.4rem] sm:leading-[4.2rem] md:leading-[5.2rem] lg:leading-[6.2rem] xl:leading-[7.4rem] font-['Unbounded'] tracking-tight  text-3xl  xl:text-[5.2rem] sm:text-[3.7rem] lg:text-[4.7rem]  h-[55vh] sm:h-screen">
           <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]">
          Hartaj Writes , blog by <span className="text-slate-200"> Hartaj Singh</span> </div>
           <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]" > <span className="text-slate-200 underline decoration-[#F9AA5E] decoration-4 md:decoration-8 underline-offset-4">  Straight Forward </span> and Simple
@@ -26,6 +27,7 @@ const Home = () => {
           
 
         </div>
+     <Blogs/> 
       </div>
     </>
   );
---
+++ components/Blogs.tsx
@@ -0,0 +1,19 @@
+
+const Blogs = () => {
+  return (
+    <>
+    <section className="w-full min-h-[50vh] flex flex-col justify-center items-center bg-[#071a2d]">
+        <div className="w-full min-h-[40vh] sm:w-[80vw] px-3 sm:px-0">
+            <div>
+                <h1 className="font-['Unbounded'] text-slate-200 text-2xl sm:text-5xl cursor-pointer"> Blog Posts: <span className="hover:opacity-80"> #</span> </h1>
+                <hr className="w-full mt-2 underline decoration-slate-100 decoration-4 underline-offset-4"></hr>
+               
+            </div>
+        </div>
+    </section>
+    
+    </>
+  )
+}
+
+export default Blogs
