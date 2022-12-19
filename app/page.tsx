2db2ec115067b89d2db16d014c8be17a9d484202
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-19 19:40:22 +0530

    Giving final touches

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ const Home = () => {
             </h1>{" "}
           </div>
         </div>
-        <div className="bg-[#0e2640] px-3 w-full flex flex-col justify-center items-center text-slate-400 font-bold leading-[3.3rem] sm:leading-[4.2rem] md:leading-[5.2rem] lg:leading-[6.2rem] xl:leading-[7.2rem] font-['Unbounded'] tracking-tight  text-3xl  xl:text-[5.5rem] sm:text-[3.7rem] lg:text-[4.7rem]  h-[55vh] sm:h-[90vh]">
+        <div className="bg-[#0e2640] px-3 w-full flex flex-col justify-center items-center text-slate-400 font-bold leading-[3.4rem] sm:leading-[4.2rem] md:leading-[5.2rem] lg:leading-[6.2rem] xl:leading-[7.4rem] font-['Unbounded'] tracking-tight  text-3xl  xl:text-[5.2rem] sm:text-[3.7rem] lg:text-[4.7rem]  h-[55vh] sm:h-[90vh]">
           <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]">
          Hartaj Writes , blog by <span className="text-slate-200"> Hartaj Singh</span> </div>
-          <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]" > <span className="text-slate-200">  Straight Forward </span> and Simple
+          <div className="w-full mx-1 sm:mx-0 sm:w-[80vw]" > <span className="text-slate-200 underline decoration-[#F9AA5E] decoration-4 md:decoration-8 underline-offset-4">  Straight Forward </span> and Simple
          <span className="text-slate-300"> articles</span>  </div> 
-            <div className="w-full mx-1 sm:mx-0 sm:w-[80vw] "> on <span className="text-slate-200"> Web Development</span> and <span className="text-slate-200"> Computer Science</span>
+            <div className="w-full mx-1 sm:mx-0 sm:w-[80vw] "> on <span className="text-slate-200 underline decoration-[#F9AA5E] decoration-4 md:decoration-8 underline-offset-4 "> Web Development</span> and <span className="text-slate-200"> Computer Science</span>
           </div>
           
 
