c0b3efbe569de55287c4b7ffe53daa36d57a319d
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-20 15:17:52 +0530

    adding Loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+        <h1 className="text-9xl text-slate-200">Loading ..</h1>
+    </div>
+    </>
+  )
+}
+
+export default loading
--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -1,5 +1,25 @@
+import { GetStaticProps, NextPage } from "next"
+import fs from "fs"
+import matter from "gray-matter"
 
-const Blogs = () => {
+interface ArticleMeta {
+    title?:string
+    date?:string
+    ReadTime?:string
+    metaDesc?:string
+    PostImg?:string
+}
+
+interface PostDataTypes{
+    slug:string
+    frontmatter?:ArticleMeta[]
+}
+
+interface Iporps{
+    posts: ArticleMeta[]
+}
+
+const Blogs:NextPage<Iporps | any> = ({posts}) => {
   return (
     <>
     <section className="w-full min-h-[50vh] flex flex-col justify-center items-center bg-[#071a2d]">
@@ -9,11 +29,37 @@ const Blogs = () => {
                 <hr className="w-full mt-2 underline decoration-slate-100 decoration-4 underline-offset-4"></hr>
                
             </div>
+            <div>
+             {`${console.log(posts)}`}   
+                {/* {
+                    posts.map((post:PostDataTypes , index:number) =>{
+                        return ( <>  </>)
+                    })
+                } */}
+            </div>
         </div>
     </section>
     
     </>
   )
 }
+
+
+export const getStaticProps: GetStaticProps = async (context) =>{
+    const files = fs.readdirSync("Posts")
+    const posts = files.map((filename)=>{
+        const slug = filename.replace(".md" ,"")
+        const markdownWithMeta = fs.readFileSync(`Posts/${filename}`,"utf-8")
+    const {data: frontmatter} = matter(markdownWithMeta)
+    
+    return {
+        slug, frontmatter
+    };
+    });
+
+    return{
+        props:{posts}
+    };
+};
 
 export default Blogs
