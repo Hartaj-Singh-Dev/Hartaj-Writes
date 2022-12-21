c431bfa2b6256a710fc6fcc7d08d681d5d8ee953
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-21 20:55:09 +0530

    Let's give it shot again

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import fs from "fs"
 
+export const dynamicParams = true;
+
 export async function generateStaticParams() {  
+  const posts = fs.readdirSync("Posts") 
+
+  return [{slug:posts.map((post)=>{post.replace(".md","")})}]
 }
 
 const Blog = async () => {
