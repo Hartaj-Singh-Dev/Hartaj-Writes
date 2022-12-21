f2f848fc7f453b252afdc9648276ed2ca04ee5fa
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-21 20:33:26 +0530

    Let's give it shot

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,23 +1,7 @@
 import React from 'react'
 import fs from "fs"
 
-export async function generateStaticParams() {
-    var AllPosts
-   const posts = await fs.readdir("Posts", (err,Files)=>{
-        if(err){
-            console.log(err)
-        }else{
-           AllPosts = Files
-           
-        }
-
-          })
-          //@ts-ignore
-          return AllPosts.map((post: any)=>{
-            return {slug:post}
-          })
-
-  
+export async function generateStaticParams() {  
 }
 
 const Blog = async () => {
