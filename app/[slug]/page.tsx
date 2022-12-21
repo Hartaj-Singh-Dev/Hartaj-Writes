8eaab9b61539c9dd1334bbd580933ba15a111dbc
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-21 21:29:06 +0530

    let's try again

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,16 +1,25 @@
+
+
 import React from 'react'
 import fs from "fs"
+import { usePathname } from 'next/navigation';
+
 
 export const dynamicParams = true;
 
 export async function generateStaticParams() {  
-  const posts = fs.readdirSync("Posts") 
+  const posts = fs.readdirSync("Posts");
 
-  return [{slug:posts.map((post)=>{post.replace(".md","")})}]
+  return posts.map((post)=>{
+      return {
+        slug:post.replace(".md",""),
+      }
+    })
+  
 }
 
 const Blog = async () => {
- 
+
   return (
     <>
     </>
