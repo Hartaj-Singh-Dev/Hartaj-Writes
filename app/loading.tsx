718a75655c602cc2cd774fcd317e981373fa1b33
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-22 22:46:39 +0530

    Testing Loading UI

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,6 +5,7 @@ const loading = () => {
     <div>
     <div className="w-full h-screen flex flex-col justify-center items-center">
         <h1 className="text-9xl text-slate-200">Loading ..</h1>
+    <i className="fa-duotone fa-3x text-[#F9AA5E] animate-sping fa-loader"></i>
     </div>
     </div>
   )
