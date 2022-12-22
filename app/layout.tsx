76a0f1bf60727202145523168e5820f5df1a6dbc
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-22 21:42:53 +0530

    Again Testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" translate="no">
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
         rel="preconnect"
