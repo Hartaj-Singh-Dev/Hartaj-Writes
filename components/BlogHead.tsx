91301f5a657348037721f40c492276f290b05b82
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-23 17:57:46 +0530

    Testing on Production

--- components/BlogHead.tsx
+++ components/BlogHead.tsx
@@ -10,10 +10,10 @@ const BlogHead = ({slug , article} : any) => {
     <>
     <section className="flex flex-col justify-center items-center">
       <Link href={`/${slug}`}>
-      <div className=" my-5 w-[85vw] min-h-74 rounded-lg flex flex-col justify-evenly items-start ">
-     <div className="w-full flex flex-col justify-center items-center"> <img loading='lazy' className="w-[87vw] h-34 md:h-30 w- object-contain rounded-[1.8rem]" src={article.image} alt="" /> </div>
-        <h1 className="text-[1.5rem] py-1 text-slate-200 font-['Unbounded']">{article.title}</h1>
-        <div className='flex'><h2 className="text-slate-200 text-sm p-2 bg-[#F9AA5E] font-semibold">{article.date}</h2><h2 className=' text-sm p-2 bg-white text-[#F9AA5E] font-semibold'>{article.readTime}</h2></div>
+      <div className=" my-5 max-w-[85vw] min-h-78 rounded-lg flex flex-col lg:flex-row lg:w-auto lg:h-auto justify-evenly lg:justify-start items-start ">
+     <div className="w-full flex flex-col justify-center items-center"> <img loading='lazy' className="w-52 h-34 lg:w-52 lg:h-36  object-contain rounded-[1.8rem]" src={article.image} alt="" /> </div>
+       <div> <h1 className="text-[1.5rem]  py-1 text-slate-200 font-['Unbounded']">{article.title}</h1>    <div className='flex'><h2 className="text-slate-200 text-sm p-2 bg-[#F9AA5E] font-semibold">{article.date}</h2><h2 className=' text-sm p-2 bg-white text-[#F9AA5E] font-semibold'>{article.readTime}</h2></div>
+       </div>
       </div>
       </Link>
     </section>
