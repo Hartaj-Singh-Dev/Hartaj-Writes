34dc44edd5ef30260c77096ca11e206996db37f3
Hartaj Singh Sidhu <Hartaj Singh Sidhu>
2022-12-24 11:28:19 +0530

    giving touches

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -26,7 +26,7 @@ const Blogs: any = async () => {
     <>
     <section className="w-full min-h-[50vh] flex flex-col justify-center items-center bg-[#071a2d]">
         <div className="w-full min-h-[40vh] sm:w-[70vw] px-3 sm:px-0">
-            <div className="my-4">
+            <div className="mt-5 mb-4">
                 <h1 className="font-['Unbounded'] text-slate-200 text-2xl sm:text-5xl cursor-pointer"> Blog Posts: <span className="opacity-80 text-[1rem] sm:text-[2.5rem] hover:opacity-100"> #</span> </h1>
                 <hr className="w-full mt-2 underline decoration-slate-100 decoration-4 underline-offset-4"></hr>
                
